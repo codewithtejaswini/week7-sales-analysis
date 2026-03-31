6ea5c3f46e64fee9d23f5071f2f3a897035f75e9
Tejaswini Sawakhande <165364115+codewithtejaswini@users.noreply.github.com>
2026-03-31 17:39:35 +0530

    Implement report generation in reporter.py
    Added functionality to generate a report using pandas and save it as an Excel file.

--- sales_analyzer/reporter.py
+++ sales_analyzer/reporter.py
@@ -1,3 +1,8 @@
-# Reporter module
-pass
+import pandas as pd
+
+def generate_report(results):
+    with pd.ExcelWriter("data/reports/report.xlsx") as writer:
+        pd.DataFrame([results]).to_excel(writer, sheet_name="Summary")
+
+    print("Report Generated")
 
