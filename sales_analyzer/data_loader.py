ff21d0dc87b31cb9a6c02f402e3cddfb44f817f9
Tejaswini Sawakhande <165364115+codewithtejaswini@users.noreply.github.com>
2026-04-02 00:03:16 +0530

    Refactor load_data function parameter and error message
    Updated parameter name and improved error handling.

--- sales_analyzer/data_loader.py
+++ sales_analyzer/data_loader.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-def load_data(file_path):
+def load_data(path):
     try:
-        df = pd.read_csv(file_path)
+        df = pd.read_csv(path)
         print("Data Loaded Successfully")
+        print("\n📌 Columns:", df.columns)
         return df
     except Exception as e:
-        print("Error:", e)
-        return None
+        print("❌ Error loading data:", e)
+        exit()
