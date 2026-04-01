abcfa39d6f39f9f53b8139da48a73315e88c9b76
Tejaswini Sawakhande <165364115+codewithtejaswini@users.noreply.github.com>
2026-04-02 00:04:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from sales_analyzer import (
-    load_data,
-    clean_data,
-    analyze_data,
-    create_charts,
-    generate_report
-)
+from sales_analyzer.data_loader import load_data
+from sales_analyzer.data_cleaner import clean_data
+from sales_analyzer.analyzer import analyze_data
+from sales_analyzer.visualizer import create_charts
+from sales_analyzer.reporter import generate_report
+
 
 def main():
-    file_path = "data/raw/sales_data.csv"
+    df = load_data("data/raw/sales_data.csv")
 
-    df = load_data(file_path)
     df = clean_data(df)
 
-    results = analyze_data(df)
+    result = analyze_data(df)
 
-    create_charts(df)
-    generate_report(results)
+    create_charts(result["df"])
+
+    generate_report(result)
+
 
 if __name__ == "__main__":
     main()
